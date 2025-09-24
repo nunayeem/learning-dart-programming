e8200b3b4379785288b3a90ad0fac87484afeccf
Nizam Uddin Nayeem <nizamuddinnayeem22@gmail.com>
2025-09-24 13:55:51 +0600

    Learned three types of comments in dart

---
+++ day - 1/comments.dart
@@ -0,0 +1,17 @@
+void main()
+{
+  // This is single-line comment.
+  print("Hello, Nizam Uddin Nayeem");
+
+  /*
+    This is a multi-line comment.
+    I love Coding
+    I like dart programming language
+    Dart is Super easy to learn
+  */
+  print("This is multi-line comment");
+
+  /// Documentation Comment in dart
+
+
+}
