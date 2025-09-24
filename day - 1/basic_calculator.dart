3b17dd06642052399bb3989dd7d3792b738ced8a
Nizam Uddin Nayeem <nizamuddinnayeem22@gmail.com>
2025-09-24 13:04:11 +0600

    Basic Calculator with dart programming

---
+++ day - 1/basic_calculator.dart
@@ -0,0 +1,16 @@
+void main()
+{
+  int numberOne = 20;
+  int numberTwo = 30;
+
+  int sum = numberOne + numberTwo;
+  int substraction = numberOne - numberTwo;
+  int multi = numberOne * numberTwo; 
+  double division = numberOne / numberTwo; 
+
+  print("Sum of two number is $sum");
+  print("Substraction of two number is $substraction");
+  print("Multiplication of two number is $multi");
+  print("Divison of two number is $division");
+
+}
