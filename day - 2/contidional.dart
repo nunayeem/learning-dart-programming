e752a4a1f29fe07e68f7f608393ea403eb914823
Nizam Uddin Nayeem <nizamuddinnayeem22@gmail.com>
2025-10-07 17:36:08 +0600

    Learn Conditional in Dart

---
+++ day - 2/contidional.dart
@@ -0,0 +1,52 @@
+main()
+{
+  // If statement
+  int age = 22;
+  if(age > 18)
+  {
+    print('You are voter');
+  } 
+
+  // If else statement
+  int adult_age = 18;
+  if(adult_age > 18)
+  {
+    print('You are adult');
+  } else {
+    print('You are not adult');
+  }
+
+  // If else if statement
+  int numOfMonth = 50;
+
+  if(numOfMonth == 1) {
+    print('This is Jan Month');
+  } else if(numOfMonth == 2) {
+    print('This is Feb Month');
+  } else if(numOfMonth == 3) {
+    print('Thi is Mar Month');
+  } else if(numOfMonth == 4) {
+    print("This is Apr Month");
+  } else if(numOfMonth == 5) {
+    print('Thi is May Month');
+  } else if(numOfMonth == 6) {
+    print('This is Jun Month');
+  } else if(numOfMonth == 7) {
+    print('This is July Month');
+  } else if(numOfMonth == 8) {
+    print('This is August Month');
+  } else if(numOfMonth == 9) {
+    print('This is Sep Month');
+  } else if(numOfMonth == 10) {
+    print('This is Oct Month');
+  } else if(numOfMonth == 11) {
+    print('This is Nov Month');
+  } else if(numOfMonth == 12) {
+    print('This is Dec Month');
+  } else {
+    print("The Month number is is invalid");
+  }
+
+
+
+}
